0148b4322c38db659678cc8ba41a4b91bdfda606
Yong Chen <Monstercoder@gmail.com>
2011-01-25 23:19:11 -0600

    created solution

---
+++ TwitterStudio/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Company.TwitterStudio
+{
+    static class GuidList
+    {
+        public const string guidTwitterStudioPkgString = "3ec16d46-a7b5-4f14-9d8d-2853e7c6d342";
+        public const string guidTwitterStudioCmdSetString = "59a1ad52-0f4d-4b78-bced-67bd69636e86";
+        public const string guidToolWindowPersistanceString = "850f1f53-1e1e-4332-b65b-17a16ec9f430";
+
+        public static readonly Guid guidTwitterStudioCmdSet = new Guid(guidTwitterStudioCmdSetString);
+    };
+}
---
+++ TwitterStudio/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TwitterStudio")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Company")]
+[assembly: AssemblyProduct("TwitterStudio")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("TwitterStudio_IntegrationTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001000b48638c50a32afa7fa101710370e8ce2b9634377eed6040a7f5bd55ef681d8300d35947b38f4e470e6d3ddad5d2b3705abe0ccba5287529cce424f90a7de19550c9e99e2d3761ee0f7061b8c02487ea2b4b1e15c95a78509b96d8ca7d87f01f05641d17c51c5bc047f7cb023eecc8859e4ce487381f32ef33899d1f089f7eba")]
+[assembly: InternalsVisibleTo("TwitterStudio_UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001000b48638c50a32afa7fa101710370e8ce2b9634377eed6040a7f5bd55ef681d8300d35947b38f4e470e6d3ddad5d2b3705abe0ccba5287529cce424f90a7de19550c9e99e2d3761ee0f7061b8c02487ea2b4b1e15c95a78509b96d8ca7d87f01f05641d17c51c5bc047f7cb023eecc8859e4ce487381f32ef33899d1f089f7eba")]
